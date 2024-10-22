b3f4f47c9e547d1925f3aa786a6ac4f1dda15114
lapkomo2018 <lapkomo2018@gmail.com>
2024-10-22 12:58:52 +0300

    fix send request in base service

--- AdminPanel/Services/BaseService.cs
+++ AdminPanel/Services/BaseService.cs
@@ -20,14 +20,27 @@ public abstract class BaseService {
         await _tokenService.WaitReadyAsync();
         request.Headers.Authorization = _tokenService.AuthorizationHeader;
         var response = await _httpClient.SendAsync(request);
+        
+        if (response.IsSuccessStatusCode) {
+            return response;
+        }
 
-        if (response.StatusCode == HttpStatusCode.Unauthorized && !await _tokenService.RefreshAsync()) {
-            throw new Exception("Failed to Authenticate");
+        if (response.StatusCode == HttpStatusCode.Unauthorized) {
+            if (!await _tokenService.RefreshAsync()) {
+                throw new Exception("Failed to Authenticate");
+            }
+            attempts += 1;
         }
         
-        request.Headers.Authorization = _tokenService.AuthorizationHeader;
-        response = await _httpClient.SendAsync(request);
-
-        return await SendRequestAsync(request, attempts - 1, response);
+        // Create a new request to avoid the disposed one
+        var newRequest = new HttpRequestMessage(request.Method, request.RequestUri) {
+            Content = request.Content,
+            Version = request.Version
+        };
+        foreach (var header in request.Headers) {
+            newRequest.Headers.Add(header.Key, header.Value);
+        }
+        
+        return await SendRequestAsync(newRequest, attempts - 1, response);
     }
 }
